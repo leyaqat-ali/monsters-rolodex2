012db9e2e956784b9e2cdd1f3fe4d419392950fd
leyaqat <leyaqat.ali@iskpro.com>
2022-06-12 02:30:32 +0530

    adding files for git hub pages

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import { CardList } from './components/card-list/card-list';
+import { SearchBox } from './components/search-box/search-box';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+      heading: "Monster Rolodex",
+      searchInput: ""
+    }
+
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({monsters: users}));
+  }
+
+  handleChange = e => {
+    this.setState({searchInput: e.target.value})
+  }
+
+  render() {
+    const {monsters, searchInput} = this.state;
+    const filteredMonsters = monsters.filter(monster => 
+      monster.name.toLowerCase().includes(searchInput.toLowerCase())
+    );
+
+    return (
+      <div className="App">
+        <h1>{this.state.heading}</h1>
+        <SearchBox placeholder='Search Monster' handleChange={this.handleChange} />
+        <CardList monsters={filteredMonsters} />
+      </div>
+    )
+  }
+}
+
+export default App;
